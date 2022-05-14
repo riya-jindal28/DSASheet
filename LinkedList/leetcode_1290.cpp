f3ed43fdb37e463a53cddd53559aaa6047d4a811
riyajindal <riyajindal@2808gmail.com>
2022-05-14 22:19:45 +0530

    Sorting and Searching Easy Questions, Linked List Questions

---
+++ LinkedList/leetcode_1290.cpp
@@ -0,0 +1,18 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int dec=0;
+        while(head){
+            dec=dec*2 + head->val;
+            head=head->next;
+        }
+        return dec;
+    }
+};
